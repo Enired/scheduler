4d3fb21b85590076b19ff75c7cb3e38128a6ad6f
Derine Banis <d.banis.13@gmail.com>
2022-07-19 21:42:10 -0700

    Add test for Application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import { render, cleanup, waitForElement, prettyDOM, getByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -10,10 +10,17 @@ describe('Application', ()=>{
   xit("renders without crashing", () => {
     render(<Application />);
   });
+
   it("defaults to Monday and changes the schedule when a new day is selected", ()=>{
     const {getByText} = render(<Application/>);
 
     return waitForElement(()=>getByText('Monday'))
+  })
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async ()=>{
+    const {container} = render(<Application/>)
+    await waitForElement(()=>getByText(container, 'Archie Cohen'))
+    console.log(prettyDOM(container))
   })
 
 })
