f3fc81b646cf77aa420a950464068f44bb11ae08
Derine Banis <d.banis.13@gmail.com>
2022-07-19 20:45:59 -0700

    Add test for Appointments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,9 +10,9 @@ export const useApplicationData = () => {
 
  
  //AXIOS REQUEST
- const daysUrl = "http://localhost:8001/api/days"
- const appointmentsUrl = "http://localhost:8001/api/appointments"
- const interviewerURL = "http://localhost:8001/api/interviewers"
+ const daysUrl = "/api/days"
+ const appointmentsUrl = "/api/appointments"
+ const interviewerURL = "/api/interviewers"
  
   useEffect(()=>{
     const getDays = axios.get(daysUrl)
