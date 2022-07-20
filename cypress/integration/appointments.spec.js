a0aa52be5f2f26e34f25ec183f4f97ede977a686
Derine Banis <d.banis.13@gmail.com>
2022-07-20 16:54:26 -0700

    Complete test for editing interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,9 +1,10 @@
 describe('Appointments', ()=>{
-  
-  it("should book an interview", () => {
+  beforeEach(()=>{
     cy.request('GET','/api/debug/reset');
     cy.visit("/");
     cy.contains("Monday");
+  })
+  it("should book an interview", () => {
     cy.get('[alt=Add]')
       .first()
       .click();
@@ -15,6 +16,24 @@ describe('Appointments', ()=>{
       .click()
     cy.contains('Save')
       .click()
+    cy.contains('.appointment__card--show','Lydia Miller-Jones')
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
   });
+
+  it('should edit an interview', ()=>{
+    cy.contains('[data-testid=appointment]', 'Archie Cohen')
+    cy.get('[alt=Edit]')
+      .first()
+      .click({force:true})
+    cy.get('[alt="Tori Malcolm"]')
+      .click()
+    cy.get('input')
+      .clear()
+      .type('Octavio Silva')
+    cy.contains('Save')
+      .click()
+    cy.contains('.appointment__card--show','Octavio Silva')
+    cy.contains('.appointment__card--show', 'Tori Malcolm');
+  })
 
 })
