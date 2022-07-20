47af8c958f901607224327a7e1336683511622ac
Derine Banis <d.banis.13@gmail.com>
2022-07-19 23:04:14 -0700

    Complete implementation of test for Application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, prettyDOM, getByText } from "@testing-library/react";
+import { render, cleanup, waitForElement, prettyDOM, getByText, getAllByTestId, fireEvent, getByPlaceholderText, getByAltText, getByTestId, debugDOM, queryByText} from "@testing-library/react";
 
 import Application from "components/Application";
+
 
 afterEach(cleanup);
 
@@ -18,10 +19,38 @@ describe('Application', ()=>{
   })
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async ()=>{
-    const {container} = render(<Application/>)
-    await waitForElement(()=>getByText(container, 'Archie Cohen'))
-    console.log(prettyDOM(container))
+    const {container, debug} = render(<Application/>)
+    await waitForElement(()=> getByText(container, "Archie Cohen"))
+    const appointments =  getAllByTestId(container, "appointment")
+    const appointment = appointments[0]
+
+
+    fireEvent.click(getByAltText(appointment, 'Add'))
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i),{
+      target: {value:"Lydia Miller-Jones"}
+    })
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"))
+
+    fireEvent.click(getByText(appointment, "Save"))
+
+    expect(getByText(appointment, 'SAVING')).toBeInTheDocument()
+
+    await waitForElement(()=>getByText(appointment, 'Lydia Miller-Jones'))
+
+    expect(getByText(appointment, 'Lydia Miller-Jones')).toBeInTheDocument()
+
+    const day = getAllByTestId(container, 'day').find(day=>queryByText(day, "Monday"))
+
+    expect(getByText(day, 'no spots remaining')).toBeInTheDocument()
+
+
+
+
+
   })
+
+
 
 })
 
